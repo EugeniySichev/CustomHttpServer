9e1054a3235a0e8ffbb2f4654d5e7eff6aff56d3
EugeniySichev <sichev91@inbox.ru>
2021-04-24 22:22:41 +0300

    moved part of the functionality into a handleClient method

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,7 +5,6 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -13,58 +12,30 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class Main {
-
     public static void main(String[] args) {
-	new Server().bootstrap();
+        new Server().bootstrap();
     }
 }
 
-class Server{
+class Server {
     private final static int BUFFER_SIZE = 256;
     private AsynchronousServerSocketChannel server;
 
     private final static String HEADERS =
             "HTTP/1.1 200 OK\n" +
-            "Server: Test\n" +
-                "Content-Type: text/html\n" +
-                "Content-Length: %s\n" +
-                "Connection: close\n\n";
+                    "Server: naive\n" +
+                    "Content-Type: text/html\n" +
+                    "Content-Length: %s\n" +
+                    "Connection: close\n\n";
 
-    public void bootstrap(){
+    public void bootstrap() {
         try {
             server = AsynchronousServerSocketChannel.open();
-            server.bind(new InetSocketAddress("127.0.0.1", 8080));
+            server.bind(new InetSocketAddress("127.0.0.1", 8088));
 
-            Future<AsynchronousSocketChannel> future = server.accept();
-            System.out.println("new client thread");
-
-            AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
-
-            while (clientChannel != null && clientChannel.isOpen()){
-                ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-                StringBuilder builder = new StringBuilder();
-                boolean keepReading = true;
-
-                while(keepReading) {
-                    clientChannel.read(buffer).get();
-
-                    int position = buffer.position();
-                    keepReading = position == BUFFER_SIZE;
-
-                    byte[] array = keepReading
-                            ? buffer.array()
-                            : Arrays.copyOfRange(buffer.array(), 0, position);
-
-                    builder.append(new String(buffer.array()));
-                    buffer.clear();
-                }
-
-                String body = "<html><h1>Test test test</h1><html>";
-                String page = String.format(HEADERS, body.length()) + body;
-                ByteBuffer resp = ByteBuffer.wrap(page.getBytes());
-                clientChannel.write(resp);
-
-                clientChannel.close();
+            while (true) {
+                Future<AsynchronousSocketChannel> future = server.accept();
+                handleClient(future);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -74,6 +45,40 @@ class Server{
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Future<AsynchronousSocketChannel> future)
+            throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        System.out.println("new client thread");
+
+        AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
+
+        while (clientChannel != null && clientChannel.isOpen()) {
+            ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+            StringBuilder builder = new StringBuilder();
+            boolean keepReading = true;
+
+            while (keepReading) {
+                clientChannel.read(buffer).get();
+
+                int position = buffer.position();
+                keepReading = position == BUFFER_SIZE;
+
+                byte[] array = keepReading
+                        ? buffer.array()
+                        : Arrays.copyOfRange(buffer.array(), 0, position);
+
+                builder.append(new String(array));
+                buffer.clear();
+            }
+
+            String body = "<html><body><h1>Test test test</h1></body></html>";
+            String page = String.format(HEADERS, body.length()) + body;
+            ByteBuffer resp = ByteBuffer.wrap(page.getBytes());
+            clientChannel.write(resp);
+
+            clientChannel.close();
         }
     }
 }
